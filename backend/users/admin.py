7763e765b8317ef516a6cd2e86113a6543ed0891
niravGanatra <niravganatra09@gmail.com>
2025-12-28 14:54:49 +0530

    feat(admin): register CareerApplication in Django admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Address
+from .models import User, Address, CareerApplication
 
 admin.site.register(User, UserAdmin)
 
@@ -9,3 +9,10 @@ class AddressAdmin(admin.ModelAdmin):
     list_display = ['user', 'address_type', 'full_name', 'city', 'state', 'is_default']
     list_filter = ['address_type', 'is_default', 'state']
     search_fields = ['user__username', 'full_name', 'city', 'pincode']
+
+@admin.register(CareerApplication)
+class CareerApplicationAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'phone', 'created_at', 'resume']
+    list_filter = ['created_at']
+    search_fields = ['full_name', 'email', 'phone']
+    readonly_fields = ['created_at']
